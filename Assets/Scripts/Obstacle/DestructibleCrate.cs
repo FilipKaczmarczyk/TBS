d466e23bf44f7c8bc82acde87815b1dedfe2409e
HappyArroW <stifmeister0700@gmail.com>
2023-04-18 19:02:29 +0200

    Add destructible crate parts

--- Assets/Scripts/Obstacle/DestructibleCrate.cs
+++ Assets/Scripts/Obstacle/DestructibleCrate.cs
@@ -1,12 +1,15 @@
 using System;
 using Grid;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 namespace Obstacle
 {
     public class DestructibleCrate : MonoBehaviour
     {
         public static event EventHandler OnAnyDestroyed;
+
+        [SerializeField] private Transform destroyedCratePrefab;
         
         private GridPosition _gridPosition;
 
@@ -22,9 +25,27 @@ namespace Obstacle
         
         public void Damage()
         {
+            var crateDestroyedTransform = Instantiate(destroyedCratePrefab, transform.position, transform.rotation);
+            
+            var randomDir = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f));
+            ApplyExplosionToChildren(crateDestroyedTransform, 200f, transform.position + randomDir, 10f);
+            
             Destroy(gameObject);
             
             OnAnyDestroyed?.Invoke(this, EventArgs.Empty);
+        }
+        
+        private void ApplyExplosionToChildren(Transform root, float explosionForce, Vector3 explosionPosition, float explosionRadius)
+        {
+            foreach (Transform child in root)
+            {
+                if (child.TryGetComponent<Rigidbody>(out var childRigidbody))
+                {
+                    childRigidbody.AddExplosionForce(explosionForce, explosionPosition, explosionRadius);
+                }
+            
+                ApplyExplosionToChildren(child, explosionForce, explosionPosition, explosionRadius);
+            }
         }
     }
 }
